0ca8c5c2e2ca45dbd2ab6ab04aefcf67f45e5890
Marcos Vieira <marcosvieira482@gmail.com>
2023-11-14 19:55:24 -0300

    feat: update javascript game code

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Funcção para reiniciar o jogo
+    // Função para reiniciar o jogo
 
     function restartGame() {
         currentScore = 0;
@@ -94,4 +94,97 @@ document.addEventListener('DOMContentLoaded', () => {
             cell.classList.add('new-tile');
         }
     }
-})
+
+    //Função para mover os quadrados baseado nas teclas do teclado
+    function move(direction) {
+        let hasChanged = false;
+        if (direction === 'ArrowUp' || direction === 'ArrowDown') {
+            for (let j = 0; j < size; j++) {
+                const column = [...Array(size)].map((_, i) => board[i][j]);
+                const newColumn = transform(column, direction === 'ArrowUp');
+                for (let i = 0; i < size; i++) {
+                    if (board[i][j] != newColumn[i]) {
+                        hasChanged = true;
+                        board[i][j] = newColumn;
+
+                    }
+                }
+            }
+        } else if (direction === 'ArrowLeft' || direction === 'ArrowRight') {
+            for (let i = 0; i < size; i++) {
+                const row = board[i];
+                const newRow = transform(row, direction === 'ArrowLeft');
+                if (row.join(',') != newRow.join(',')) {
+                    hasChanged = true;
+                    board[i] = newRow
+                }
+            }
+        }
+
+        if (hasChanged) {
+            placeRandon();
+            renderBoard();
+            checkGameOver();
+        }
+    }
+
+    //Função para ptransformar a linha (linha ou coluna) baseado na direção do movimento
+
+    function transform(line, moveTowardsStart) {
+        let newLine = line.filter(cell => cell !== 0);
+        if (!moveTowardsStart) {
+            newLine.reverse();
+
+        }
+
+        for (let i = 0; i < newLine.length - 1; i++) {
+            if (newLine[i] === newLine[i + 1]) {
+                newLine *= 2;
+                //Atualiza a pontuação qunado os quadrados são unidos
+                updateScore(newLine[i]);
+                newLine.splice(i + 1, 1);
+            }
+        }
+        while (newLine.length < size) {
+            newLine.push(0);
+        }
+
+        if (!moveTowardsStart) {
+            newLine.reverse();
+        }
+        return newLine
+    }
+
+    //Função para checar se o jogo acabou
+    function checkGameOver() {
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                if (board[i][j] === 0) {
+                    //Enquanto existir uma celula vazia o jogo não acaba
+                    return;
+                }
+                if (j < size - 1 && board[i][j] === board[i][j + 1]) {
+                    //Enquanto houver celulas horizontais adjacentes iguais, o movimento ainda é possível
+                    return;
+                }
+
+                if (i < size - 1 && board[i][j] === board[i + 1][j]) {
+                    //Enquanto houver celulas verticais adjacentes iguais, o movimento ainda é possível
+                    return;
+                }
+            }
+        }
+        //Se chegar até aqui, não existem mais movimentos possíveis, então o jogo acabou
+        gameOverElement.style.display = 'flex';
+    }
+
+    //Evests listeners
+    document.addEventListener('keydown', event => {
+        if (['ArrowUp'], ['ArrowDown'], ['ArrowLeft'], ['ArrowRight'].includes(event.key)) {
+            move(event.key);
+        }
+    });
+    document.getElementById('restart-btn').addEventListener('click', restartGame);
+
+    initializeGame();
+});
